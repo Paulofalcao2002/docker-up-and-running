04779bc42794d4a2648f3739610608855916acf2
Paulo <paulohenriquemoreirafalcao@gmail.com>
2024-05-18 20:56:15 -0300

    chapter 3: Installing docker

---
+++ chapter-3/commands.sh
@@ -0,0 +1,8 @@
+# Run a ubuntu linux docker container
+docker container run --rm -ti docker.io/ubuntu:latest /bin/bash
+
+# Run a fedora linux docker container
+docker container run --rm -ti docker.io/fedora:latest /bin/bash
+
+# Run a alpine linux docker container
+docker container run --rm -ti docker.io/alpine:latest /bin/sh
